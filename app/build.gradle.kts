cfdb3f4524a66af4dea5cd5120d42abde5327480
coelacanth9 <coelacanth9@gmail.com>
2026-01-27 09:32:09 +0900

    feat: Google Play Billing Library 依存関係追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.google.billing)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
