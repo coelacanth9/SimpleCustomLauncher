c5cd2408d55dc34fdfe7add18d4659ad0a34050c
coelacanth9 <coelacanth9@gmail.com>
2026-01-27 11:52:36 +0900

    パッケージ名修正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.simplecustomlauncher"
+        applicationId = "com.coelacanth9.simplecustomlauncher"
         minSdk = 33
         targetSdk = 36
         versionCode = 1
